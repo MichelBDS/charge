07370497461f2b18e67c07cbce981af0eb7b7bdc
MichelBDS <michel.barros94@gmail.com>
2025-08-13 16:05:14 -0300

    Menu lateral e ajuste da tela home

--- src/app/modules/charge/pages/home/home.component.ts
+++ src/app/modules/charge/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MonthlyPaymentComponent } from '../../components/monthly-payment/monthly-payment.component';
 import { QuickAccessComponent } from '../../components/quick-access/quick-access.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, QuickAccessComponent],
+  imports: [HeaderComponent, QuickAccessComponent,SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
